ce55ccb761181edcf9b5b603139d04ccab1cf31e
v.marchenko <v.marchenko@m-games-ltd.com>
2018-06-07 00:15:38 +0300

    forecast parse not completed

--- app/src/main/java/com/example/vladimir/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/vladimir/weatherapp/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.vladimir.weatherapp;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 import com.example.vladimir.weatherapp.OpenApi.OpenApi;
 import com.example.vladimir.weatherapp.models.City;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -43,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<City> adapter =
                 new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, new ArrayList<>(addedCities));
-        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -52,11 +55,31 @@ public class MainActivity extends AppCompatActivity {
                 if (spinner.getSelectedItem() != null) {
                     openApi.getCityData((City) spinner.getSelectedItem(),
                             OpenApi.Data.WEATHER,
-                            response -> weather.setText(response.toString()),
+                            response -> {
+                                try {
+                                    weather.setText("Weather is " + ((JSONObject) response.optJSONArray("weather").get(0)).optString("main") +
+                                            " temperature is " + response.optJSONObject("main").optString("temp"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            },
                             error -> Toast.makeText(getApplicationContext(), "count not load weather data " + error.toString(), Toast.LENGTH_SHORT).show());
                     openApi.getCityData((City) spinner.getSelectedItem(),
                             OpenApi.Data.FORECAST,
-                            response -> forecast.setText(response.toString()),
+                            response -> {
+                                forecast.setText(response.toString());
+                                List<Date> dateList = new ArrayList<>();
+                                for (int j = 0; j < 23; j++) {
+                                    try {
+                                        dateList.add(new Date(Integer.parseInt(((JSONObject) response.optJSONArray("list").get(j)).optString("dt")) * 1000L));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                final String[] result = {""};
+                                dateList.stream().map(Date::toString).forEach(s -> result[0] += s + "\n");
+                                forecast.setText(result[0]);
+                            },
                             error -> Toast.makeText(getApplicationContext(), "count not load forecast data " + error.toString(), Toast.LENGTH_SHORT).show());
                 }
             }
